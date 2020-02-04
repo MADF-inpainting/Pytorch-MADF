113522ac83a39e44e358877130f371ae2947cd8d
cvpr2020-inpainting-anonymous <60539201+cvpr2020-inpainting-anonymous@users.noreply.github.com>
2020-02-04 17:39:11 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 ~/zhumanyu/env/bin/python train.py --train_root /data_256 --batch_size 4 --image_size 256 --use_incremental_supervision
+python train.py --train_root /data_256 --batch_size 4 --image_size 256 --use_incremental_supervision
