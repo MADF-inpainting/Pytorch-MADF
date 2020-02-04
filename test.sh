f32975a16fa209869097c0076f98d65b14f97c61
cvpr2020-inpainting-anonymous <60539201+cvpr2020-inpainting-anonymous@users.noreply.github.com>
2020-02-04 17:39:58 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,1 @@
-#ln -s -f ../testset/place2_testset/images_1k/
-#ln -s -f ../testset/place2_testset//
-ln -s -f ../../dataset/val_large_1k
-ln -s -f ../../dataset/mask_reverse/testing_mask_reverse_dataset/
-ln -s -f ../../testset/place2_testset/test_large_1.2w
-rm result_place2/*
-rm gt_result_place2/*
-#export LD_LIBRARY_PATH=~/zhumanyu/cuda-9.0/lib64/:~/zhumanyu/cudnn-7.1/cuda/lib64/:$LD_LIBRARY_PATH
-CUDA_VISIBLE_DEVICES=1 ~/zhumanyu/env/bin/python test.py
+python test.py
